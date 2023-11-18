7e72af0c364367e3651ae589e430bb8df5b58166
Onur Arıkan <onur93arikan@gmail.com>
2023-11-19 00:12:47 +0300

    Change teams to tahmin

--- f1t/apps/tahmin/urls.py
+++ f1t/apps/tahmin/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('<str:series>/<int:year>/', views.RaceListView.as_view(), name='race_list'),
     path('<str:series>/<int:year>/last/', views.LastRaceRedirectView.as_view(), name='redirect_last_race'),
     path('<str:series>/<int:year>/<int:round>/', views.RaceTahminView.as_view(), name='race_tahmins'),
-    path('<str:series>/<int:year>/teams/', views.TeamListView.as_view(), name='team_list'),
-    path('<str:series>/<int:year>/teams/new/', views.NewTahminView.as_view(), name='new_team_form'),
+    path('<str:series>/<int:year>/tahmin/', views.TeamListView.as_view(), name='team_list'),
+    path('<str:series>/<int:year>/tahmin/new/', views.NewTahminView.as_view(), name='new_team_form'),
 ]
