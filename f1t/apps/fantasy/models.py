9c057effdcec9f58460bc9856900ea1b5eaec975
Onur Arıkan <onur93arikan@gmail.com>
2024-02-22 15:26:45 +0300

    Add str for Rating model

--- f1t/apps/fantasy/models.py
+++ f1t/apps/fantasy/models.py
@@ -443,3 +443,6 @@ class Rating(models.Model):
     score = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     onur = models.PositiveSmallIntegerField(null=True, blank=True)
     semih = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.race} Race Rating"
