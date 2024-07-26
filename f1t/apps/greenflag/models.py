5434babd692edc07b601b89611b813a79dde1f8a
Onur Arikan <onur.arikan@mrc-tr.com>
2024-07-26 17:01:22 +0300

    Add title to Green Flag

--- f1t/apps/greenflag/models.py
+++ f1t/apps/greenflag/models.py
@@ -4,10 +4,11 @@ from django.urls import reverse
 from f1t.apps.fantasy.models import Race
 class GreenFlag(models.Model):
     race = models.OneToOneField(Race, on_delete=models.CASCADE, related_name='greenflag_instance')
+    title = models.CharField(max_length=255)
     url = models.URLField()
 
     def __str__(self):
-        return f"{self.race} Green Flag Sayısı"
+        return f"{self.race} Green Flag: {self.title}"
 
     def get_absolute_url(self):
         return reverse('greenflag:greenflag_detail', args=[self.pk])
