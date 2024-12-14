d44fcd04689267197baf2a5163ddd727d8c3556c
Onur Arıkan <onur93arikan@gmail.com>
2024-12-14 14:22:08 +0300

    Update script no longer will work outside of a virtual environment

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,16 @@
 #! /usr/bin/env bash
 set -e
 
+# Check if the script is running inside a virtual environment
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Error: This script must be run inside a virtual environment." >&2
+    exit 1
+fi
+
+scripts/copydb.py
 git pull
 pip install -r requirements/production.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py test --settings=f1t.settings.tests
 scripts/reload.sh
-
